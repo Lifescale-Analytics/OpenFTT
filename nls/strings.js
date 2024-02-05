4c95b4e78544af34c856f3810e622d15692fbfbb
Tom O'Toole <tomotoole@lifescaleanalytics.com>
2024-02-05 14:53:08 -0500

    Initial widget code reorganization.

---
+++ nls/strings.js
@@ -0,0 +1,9 @@
+define({
+  root: {
+    _widgetLabel: 'Open Fault Trace Tool',
+    widgetTitle: 'Open Fault Trace Tool',
+    description: 'Open Fault Trace Tool'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
